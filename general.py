d597cc17f0832c958cb72b0578fd16773e98065c
Shivanshu <developer.shivanshu@gmail.com>
2019-12-01 13:01:19 +0530

    Python Web Crawler

---
+++ general.py
@@ -0,0 +1,50 @@
+import os
+
+# Each website is a new website
+def create_project_dir(directory):
+    if not os.path.exists(directory):
+        print('Creating a Project ' + directory)
+        os.makedirs(directory)
+
+# Create queue and crawled files
+def create_data_files(project_name, base_url):
+    queue = project_name + '/queue.txt'
+    crawled = project_name + '/crawled.txt'
+    if not os.path.isfile(queue):
+        write_file(queue, base_url)
+
+    if not os.path.isfile(crawled):
+        write_file(crawled, '')
+
+
+# Create a new file
+def write_file(path, data):
+    f = open(path, 'w')
+    f.write(data)
+    f.close()
+
+# Add data to existing file
+def append_to_file(path, data):
+    with open(path, 'a') as file:
+        file.write(data + '\n')
+
+# Delete the content in file
+def delete_file_content(path):
+    with open(path, 'w'):
+        # Do nothing
+        pass
+
+# Read a file and convert each line to set items
+def file_to_set(file_name):
+    results = set()
+    with open(file_name, 'rt') as f:
+        for line in f:
+            results.add(line.replace('\n', ''))
+    return results
+
+# Iterate to a set , each item will be a new line  in the file
+def set_to_file(links, file):
+    # Delete the old data
+    delete_file_content(file)
+    for link in sorted(links):
+        append_to_file(file, link)
